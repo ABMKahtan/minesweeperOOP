9e2fd811608d18f5520479b0ed0c992e2a975b72
Abu Baker Kahtan <abumkahtan@gmail.com>
2021-11-22 23:12:05 +0000

    added whats left of the attributes and methods to board and empty tile

--- src/main/java/com/tsi/training/kahtan/minesweeperoop/Board.java
+++ src/main/java/com/tsi/training/kahtan/minesweeperoop/Board.java
@@ -1,7 +1,21 @@
 package com.tsi.training.kahtan.minesweeperoop;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Board {
     // This class is going to make the board as well as define what each tile is for the minesweeper game.
+    //////////////////Attributes//////////////////
+    private List<Tile> gameTiles = new ArrayList<Tile>();
+    private int boardSize;
+
+
+    //////////////////Constructors////////////////
+
+
+
+    //////////////////Methods/////////////////////
+
 
 
 }
--- src/main/java/com/tsi/training/kahtan/minesweeperoop/EmptyTile.java
+++ src/main/java/com/tsi/training/kahtan/minesweeperoop/EmptyTile.java
@@ -1,4 +1,20 @@
 package com.tsi.training.kahtan.minesweeperoop;
 
-public class EmptyTile {
+public class EmptyTile extends Tile{
+
+    //////////////////Attributes//////////////////
+    private int counter;
+
+
+
+    //////////////////Constructors////////////////
+
+
+
+    //////////////////Methods/////////////////////
+
+
+    public void setCounter(int counter) {
+        this.counter = counter;
+    }
 }
